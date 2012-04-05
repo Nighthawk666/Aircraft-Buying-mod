cf007aced8e5de27ed08a84c961c5ebef15668d0
Gordon Nighthawk <caramon_majere@gmx.de>
2012-04-05 19:09:27 +0200

    All Files 0.91

---
+++ Step1 Aircraft buying Mod Complete 0.91/SQL.sql
@@ -0,0 +1,6 @@
+alter table `phpvms_aircraft` add column `price` varchar(15) NOT NULL DEFAULT '0';
+alter table `phpvms_aircraft` add column `cond` varchar(15) NOT NULL DEFAULT '0';
+alter table `phpvms_aircraft` add column `bought` varchar(15) NOT NULL DEFAULT '0';
+alter table `phpvms_aircraft` add column `rep` varchar(15) NOT NULL DEFAULT '0';
+alter table `phpvms_aircraft` add column `maint` varchar(15) NOT NULL DEFAULT '0';
+alter table `phpvms_aircraft` add column `rmtime` varchar(15) NOT NULL DEFAULT '0';
